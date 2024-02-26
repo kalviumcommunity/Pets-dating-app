c5945bdd413f8ca0d86a33717380a0b064a06de9
yashvx <144120010+yashvx@users.noreply.github.com>
2024-02-26 17:51:33 +0530

    Revert "MongoDB connected"

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 const express = require('express');
-const connection = require('./Config/db');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
-
-const PORT = process.env.PUBLIC_PORT || 3000;
-
 
 app.get('/ping', (req, res) => {
     res.send('Pong!');
 });
 
-
-
-app.listen(PORT, async() => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     try {
-        await connection;
-        console.log("Connected to DB")
+        console.log(`Server is running on port ${PORT}`); 
     } catch (error) {
         console.log(error)
     }
-
-    console.log(`Server is running on port ${PORT}`); 
 });
