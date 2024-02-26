ec844db16bf7c31db765a56c962bd1457e3a0919
yashvx <yashvdadhich04@gmail.com>
2024-02-26 17:49:26 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const connection = require('./Config/db');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PUBLIC_PORT || 3000;
+
 
 app.get('/ping', (req, res) => {
     res.send('Pong!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+
+app.listen(PORT, async() => {
     try {
-        console.log(`Server is running on port ${PORT}`); 
+        await connection;
+        console.log("Connected to DB")
     } catch (error) {
         console.log(error)
     }
+
+    console.log(`Server is running on port ${PORT}`); 
 });
